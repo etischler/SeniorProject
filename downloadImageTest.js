95613bdd5335fd4eadeee7c387d5da169f0b33a5
Ed Tischler <etischler@ufl.edu>
2017-04-17 18:20:04 -0400

    backend minus aws

--- downloadImageTest.js
+++ downloadImageTest.js
@@ -2,20 +2,101 @@ http = require('http');
 fs = require('fs');
 qs = require('querystring');
 cmd=require('node-cmd');
+request = require('request');
 
  function getClientAddress(req) {
         return (req.headers['x-forwarded-for'] || '').split(',')[0] 
         || req.connection.remoteAddress;
 };
 
-function myCallBack(dataString) { //we will now format data into json object and send back to requester
-  
+function prettyClientAddress(nonPrettyString) {
+    var index = 0;
+    //console.log(nonPrettyString[index]);
+    //console.log(nonPrettyString[index]>57);
+    while(nonPrettyString[index]!=null && (nonPrettyString.charCodeAt(index)<48 || nonPrettyString.charCodeAt(index)>57)){
+        //console.log("happens");
+        index++;
+    }
+        
+    //console.log(index);
+    var ans = nonPrettyString.substring(index,nonPrettyString.length);
+
+
+    return 'http://'+ans;
+};
+
+function myCallBack(numResponses,dataString,clientAddress) { //we will now format data into json object and send back to requester
+    //console.log("begin post json object fun");
+
+    var merchantsArr = new Array();
+    var priceArr = new Array();
+    var thumbnailsArr = new Array();
+    var linksArr = new Array();
+    var actProductName = '';
+
+    var lineSplitData = dataString.split("\n");
 
 
 
+    actProductName = lineSplitData[0];
+
+    var forLoopLength = numResponses;
+    var index = 1;
+    for(var i = 0; i < forLoopLength; i++){
+        //save thumbnail first
+        //console.log('happens foor loop');
+        thumbnailsArr.push(lineSplitData[index]);
+        index++;
+        linksArr.push(lineSplitData[index]);
+        index++;
+        priceArr.push(lineSplitData[index]);
+        index++;
+        merchantsArr.push(lineSplitData[index]);
+        index++;
+            
+    }
+    console.log('product result');
+    console.log(actProductName);
+    console.log('links:');
+    for(var i = 0;i<linksArr.length;i++)
+        console.log(linksArr[i]);
+    console.log('prices:');
+    for(var i = 0;i<linksArr.length;i++)
+        console.log(priceArr[i]);
+    console.log('merchants:');
+    for(var i = 0;i<linksArr.length;i++)
+        console.log(merchantsArr[i]);
+    console.log('not printing thumbnails due to length');
+
+    var json = JSON.stringify({ 
+        prices: priceArr, 
+        merchants: merchantsArr,
+        thumbnails: thumbnailsArr,
+        links: linksArr, 
+        product: actProductName
+    });
+
+    return json;
 
 
-}
+    /*var options = {
+      uri: 'https://www.googleapis.com/urlshortener/v1/url',//prettyClientAddress(clientAddress) //this will change to incoming ip address
+      method: 'POST',
+      json: {
+        "product": actProductName
+      }
+    };
+
+    request(options, function (error, response, body) {
+        
+      if (!error && response.statusCode == 200) {
+        console.log("status code 1 hunnit x2");
+      }
+      if(error)
+        console.log("error :(");
+    });*/
+    //console.log("done of function");
+};
 //f=fs.createWriteStream('name.jpeg');
 server = http.createServer( function(req, res) {
 
@@ -23,7 +104,7 @@ server = http.createServer( function(req, res) {
 
     if (req.method == 'POST') {
         console.log("POST");
-        console.log('Incoming post from: ' + getClientAddress(req) );
+        console.log('Incoming post from: ' + prettyClientAddress(getClientAddress(req)) );
         
         var body = '';
         req.setEncoding('binary');
@@ -55,18 +136,20 @@ server = http.createServer( function(req, res) {
         cmd.get(
             yayaReturnURL,
             function(data){
-              console.log(data);
+              //console.log(data);
               scrapperData += data;
 
-              myCallBack(scrapperData);
+              var jsonResponse = myCallBack(5,scrapperData,getClientAddress(req));
+
+              res.writeHead(200, {"Content-Type": "application/json"});
+              res.end(jsonResponse);
             }
         );
         
         
 
 
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('post received');
+        
         //server.close();
     }
     else
@@ -88,6 +171,6 @@ server = http.createServer( function(req, res) {
 });
 
 port = 3000;
-//host = '192.168.1.7';
+//host = '127.0.0.0';
 server.listen(port);
-console.log('Listening at http://' +  ':' + port);
+console.log('Listening on port ' + port);
